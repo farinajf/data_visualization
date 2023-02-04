435bd901aba07be29c6f3a3d4ed4da941eb68b9f
Fran Farina <farinajf@gmail.com>
2023-02-04 21:49:39 +0100

    first plots

--- mpl_squares.py
+++ mpl_squares.py
@@ -1,3 +1,42 @@
 import matplotlib.pyplot as plot
 
-squares = [1, 4, 9, 16, 25]
+input_values = [1, 2, 3,  4,  5]
+squares      = [1, 4, 9, 16, 25]
+
+#plot.style.use('Solarize_Light2')
+#plot.style.use('_classic_test_patch')
+#plot.style.use('bmh')
+#plot.style.use('classic')
+#plot.style.use('dark_background')
+#plot.style.use('fast')
+#plot.style.use('fivethirtyeight')
+#plot.style.use('ggplot')
+#plot.style.use('grayscale')
+#plot.style.use('seaborn')             +
+#plot.style.use('seaborn-bright')
+#plot.style.use('seaborn-colorblind')
+#plot.style.use('seaborn-dark')
+#plot.style.use('seaborn-dark-palette')
+plot.style.use('seaborn-darkgrid')
+#plot.style.use('seaborn-deep')
+#plot.style.use('seaborn-muted')
+#plot.style.use('seaborn-notebook')
+#plot.style.use('seaborn-paper')
+#plot.style.use('seaborn-pastel')
+#plot.style.use('seaborn-poster')
+#plot.style.use('seaborn-talk')
+#plot.style.use('seaborn-ticks')
+#plot.style.use('seaborn-white')
+#plot.style.use('seaborn-whitegrid')
+#plot.style.use('tableau-colorblind10')
+
+fig, ax = plot.subplots()
+ax.plot(input_values, squares, linewidth=3)
+
+ax.set_title("Números al cuadrado", fontsize=24)
+ax.set_xlabel("Número",             fontsize=14)
+ax.set_ylabel("Cuadrado",           fontsize=14)
+
+ax.tick_params(axis='both', labelsize=14)
+
+plot.show()
--- scatter_squares.py
+++ scatter_squares.py
@@ -1,8 +1,20 @@
 import matplotlib.pyplot as plot
 
+x_values = range(1, 1001)
+y_values = [x**2 for x in x_values]
+
+
 plot.style.use('seaborn-darkgrid')
 
 fig, ax = plot.subplots()
-ax.scatter(2, 4)
+ax.scatter(x_values, y_values, c=y_values, cmap=plot.cm.Blues, s=10)
+
+ax.set_title("Números al cuadrado", fontsize=24)
+ax.set_xlabel("Número",             fontsize=14)
+ax.set_ylabel("Cuadrado",           fontsize=14)
+
+ax.tick_params(axis='both', which='major', labelsize=14)
+
+ax.axis([0, 1100, 0, 1100000])
 
 plot.show()

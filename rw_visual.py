e8dfbf29667fc5e975ee02a2cdad6f7eea8ef86b
Fran Farina <farinajf@gmail.com>
2023-03-18 17:13:28 +0100

    Random walk

--- rw_visual.py
+++ rw_visual.py
@@ -2,12 +2,26 @@ import matplotlib.pyplot as plot
 
 from random_walk import RandomWalk
 
-rw = RandomWalk()
-rw.fill_walk()
+while True:
+    rw = RandomWalk(50_000)
+    rw.fill_walk()
 
-plot.style.use('classic')
+    plot.style.use('classic')
 
-fig, ax = plot.subplots()
+    fig, ax = plot.subplots(figsize=(15, 9), dpi=128)
 
-ax.scatter(rw.x_values, rw.y_values, s=15)
-plot.show()
+    point_numbers = range(rw.num_points)
+    ax.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plot.cm.Blues, edgecolors='none', s=1)
+
+    ax.scatter(0, 0, c='green', edgecolors='none', s=100)
+    ax.scatter(rw.x_values[-1], rw.y_values[-1], c='red', edgecolors='none', s=100)
+
+    #Remove axes
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
+
+    plot.show()
+
+    keep_running = input("Make another walk? (y/n): ")
+    if keep_running == 'n':
+        break

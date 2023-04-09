938f41c63e2a8647aa6ae99f2f600325ed7e629f
Fran Farina <farinajf@gmail.com>
2023-04-09 15:40:01 +0200

    COlor earthquakes

--- eq_explore_data.py
+++ eq_explore_data.py
@@ -8,8 +8,16 @@ with open(filename) as f:
 all_eq_dicts = all_eq_data['features']
 
 mags = []
+lons = []
+lats = []
 for e in all_eq_dicts:
     mag = e['properties']['mag']
+    lon = e['geometry']['coordinates'][0]
+    lat = e['geometry']['coordinates'][1]
     mags.append(mag)
+    lons.append(lon)
+    lats.append(lat)
 
 print(mags[:10])
+print(lons[:5])
+print(lats[:5])
---
+++ eq_world_map.py
@@ -0,0 +1,43 @@
+from plotly.graph_objs import Scattergeo, Layout
+from plotly import offline
+
+import json
+
+# https://github.com/ehmatthes/pcc_2e/tree/master/chapter_16/mapping_global_data_sets/data
+
+filename = 'data/eq_data_30_day_m1.json'
+
+with open(filename) as f:
+    all_eq_data = json.load(f)
+
+all_eq_dicts = all_eq_data['features']
+
+mags = []
+lons = []
+lats = []
+for e in all_eq_dicts:
+    mag = e['properties']['mag']
+    lon = e['geometry']['coordinates'][0]
+    lat = e['geometry']['coordinates'][1]
+    mags.append(mag)
+    lons.append(lon)
+    lats.append(lat)
+
+data      = [Scattergeo(lon=lons, lat=lats)]
+data      = [
+{
+ 'type': 'scattergeo',
+ 'lon' : lons,
+ 'lat' : lats,
+ 'marker' : {
+    'size'        : [5*mag for mag in mags],
+    'color'       : mags,
+    'colorscale'  : 'Viridis',
+    'reversescale': True,
+    'colorbar'    : {'title': 'Magnitude'},
+    },
+}]
+my_layout = Layout(title='Global Earthquakes')
+
+fig = {'data': data, 'layout': my_layout}
+offline.plot(fig, filename='global_earthquakes.html')
